7a7b508fe9f83d271671e481903dc8421a811f48
marcusvinics <84209835+marcusvinics@users.noreply.github.com>
2021-05-24 19:20:46 -0300

    Update q3.sh

--- CheckPoint/Permission/q3.sh
+++ CheckPoint/Permission/q3.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-# Questao 3 Other Permissions
-#Other permission, seria para outros e esta marcando octal de 644 sendo o 4 r de read, entao seria so leitura.
+#Question 3: Other Permissions
 
 echo "Resposta: So leitura"
 
