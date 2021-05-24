baeeaee0736add6877d4554dcb7e53c9d41903a4
marcusvinics <84209835+marcusvinics@users.noreply.github.com>
2021-05-24 19:24:34 -0300

    Update q3.sh

--- CheckPoint/WildCard/q3.sh
+++ CheckPoint/WildCard/q3.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-# Questao numero 3
+# Question 3: Duplicate thismonth
 
 cp /home/demo/thismonth /home/demo/letters/let1.doc && cp /home/demo/letters/let1.doc /home/demo/letters/let2.doc && cp /home/demo/letters/let1.doc /home/demo/letters/let3.doc
