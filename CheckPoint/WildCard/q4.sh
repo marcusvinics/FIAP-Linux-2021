38daff7255f70ebd31f5bae97c739f87316da1a7
marcusvinics <84209835+marcusvinics@users.noreply.github.com>
2021-05-24 19:24:51 -0300

    Update q4.sh

--- CheckPoint/WildCard/q4.sh
+++ CheckPoint/WildCard/q4.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#Questao numero 4 rename 
+#Question 4: Copy and rename
 
 cp /home/demo/letters/let?.doc /home/demo/work/misc/
 mv /home/demo/work/misc/let1.doc /home/demo/work/misc/rpt1.doc && mv /home/demo/work/misc/let2.doc /home/demo/work/misc/rpt2.doc && mv /home/demo/work/misc/let3.doc /home/demo/work/misc/rpt3.doc
