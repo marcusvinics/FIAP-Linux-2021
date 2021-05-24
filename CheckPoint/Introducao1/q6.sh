c52a545978ef5b8c6b7219de3d5aa1e93ac0f73e
marcusvinics <84209835+marcusvinics@users.noreply.github.com>
2021-05-24 19:14:37 -0300

    Update q6.sh

--- CheckPoint/Introducao1/q6.sh
+++ CheckPoint/Introducao1/q6.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-# Questáo numero 6 funciona - mas tem que coincidir com o horario do linuxzoo
+# Question 6: append
 
 cal -1 >> /home/raven/thismonth && date >> /home/raven/thismonth
 
